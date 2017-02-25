3d32ef7599c6f885cdafda4678d2d01e72841e9b
Burak Kağan Korkmaz <bkorkmaz@gyte.edu.tr>
2017-02-25 04:24:05 +0300

    [HW01] added

--- HW01/Database.java
+++ HW01/Database.java
@@ -2,41 +2,61 @@
  * Created by eksor on 24.02.2017.
  */
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class Database implements ManagementSystem{
 
     //Delimiter for CSV files
-    private static final String DELIMITER = ";";
+    private static final String DELIMITER = ",";
     //Seperator for CSV files
     private static final String SEPERATOR = "\n";
 
-    private ArrayList<String> arrayList = new ArrayList<>();
+    private static final int PERSON_ATTRIBUTES = 5;
+    private static final int BOOK_ATTRIBUTES = 6;
 
-//    protected String books[];
-//    protected int booksISBN[];
-//    protected int booksYear[];
-//    protected String booksAuthor[];
-//    protected String booksBorrower[];
-//    protected String bookLoanDate[];
-//
-//    protected String userID[];
-//    protected String userFullName[];
-//    protected String userName[];
-//    protected String userPassword[];
+    private ArrayList<Person> arrayList;
 
-
-    @Override
+    /**
+     * @param filename
+     */
     public void readFile(String filename) {
-
+        String line;
+        BufferedReader stream;
+        try {
+            stream = new BufferedReader(new FileReader(filename));
+        while ((line = stream.readLine()) != ""){
+            String token[] = line.split(DELIMITER,PERSON_ATTRIBUTES);
+            for(int i = 0; i < PERSON_ATTRIBUTES; ++i){
+                if(token[i].contains(","))
+                    throw new Error("Wrong Entry detected! Please do not use comma in the entries.");
+            }
+            Person person = new Person(token[0],token[1],token[2],token[3],token[4]);
+            arrayList.add(person);
+        }
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    @Override
+    /**
+     *
+     * @param filename
+     */
     public void writeFile(String filename) {
-
+        FileWriter stream;
+        ;
     }
 
-    @Override
+    /**
+     *
+     * @param name
+     * @param pass
+     * @return
+     */
     public String login(String name, String pass) {
         return null;
     }
--- HW01/Person.java
+++ HW01/Person.java
@@ -3,14 +3,18 @@
  */
 public class Person {
 
+    private String ID;
+    private String name;
+    private String surname;
     private String username;
     private String password;
-    private String ID;
 
-
-
-    public String getUsername() {
-        return username;
+    public Person(String ID, String name, String surname, String username, String password) {
+        setID(ID);
+        setName(name);
+        setSurname(surname);
+        setUsername(username);
+        setPassword(password);
     }
 
     public String getID() {
@@ -21,11 +25,42 @@ public class Person {
         this.ID = ID;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "| ID: " + getID() + "| Name: " + getName() + "| Surname: " + getSurname() + "| Username: " +
+                getUsername() + "| Password: " + getPassword() + " |\n";
+    }
 }
+
