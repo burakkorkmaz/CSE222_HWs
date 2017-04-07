9e7a91c5b5cc1ffa4275172623c9e3cb07433a61
Burak Kağan Korkmaz <bkorkmaz@gyte.edu.tr>
2017-04-07 21:28:32 +0300

    [HW04] hw4 updates

--- HW04/src/tr/edu/gtu/bkorkmaz/priority_queue/PriorityQueueA.java
+++ HW04/src/tr/edu/gtu/bkorkmaz/priority_queue/PriorityQueueA.java
@@ -1,7 +1,42 @@
 package tr.edu.gtu.bkorkmaz.priority_queue;
 
+import java.util.LinkedList;
+
 /**
  * Created by Burak Kağan Korkmaz on 24.03.2017.
  */
-public class PriorityQueueA {
+public class PriorityQueueA<E> extends LinkedList{
+
+    private E findMin(){
+        int size = size();
+        if(size == 0){
+            return null;
+        }
+        else{
+            E min = (E) peek();
+            for (int i = 0; i < size; i++) {
+                if(minremove(i))
+            }
+        }
+
+    }
+
+    @Override
+    public int size() {
+        return super.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return super.isEmpty();
+    }
+
+    public E deleteMin(){
+
+        return null;
+    }
+
+    public void insert(){
+
+    }
 }
